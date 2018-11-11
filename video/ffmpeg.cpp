e6c556440a67cffec3f1698aefe4ed3a7b0fcb39
Jonathan Pastor <jonathancmoa@gmail.com>
2018-11-11 19:39:46 +0100

    first commit

---
+++ video/ffmpeg.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Jonathan on 11/11/18.
+//
+
+#include "ffmpeg.h"
---
+++ video/ffmpeg.h
@@ -0,0 +1,17 @@
+//
+// Created by Jonathan on 11/11/18.
+//
+
+#ifndef GAMECLIENTSDL_FFMPEG_H
+#define GAMECLIENTSDL_FFMPEG_H
+
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <libswscale/swscale.h>
+
+class ffmpeg {
+
+};
+
+
+#endif //GAMECLIENTSDL_FFMPEG_H
