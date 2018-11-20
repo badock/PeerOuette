6e58c8ce7e6409fcc4877bcf4d0d5a7deb720fa8
badock <jonathan.pastor@me.com>
2018-11-20 02:01:34 +0100

    add directx to the list of dependencies on windows

---
+++ src/capture/capture.cpp
@@ -0,0 +1,9 @@
+//
+// Created by jonathan on 11/20/2018.
+//
+
+#include "capture.h"
+
+int capture_frame() {
+    return 0;
+}
---
+++ src/capture/capture.h
@@ -0,0 +1,12 @@
+//
+// Created by jonathan on 11/20/2018.
+//
+
+#ifndef GAMECLIENTSDL_CAPTURE_H
+#define GAMECLIENTSDL_CAPTURE_H
+
+#include <d3d11.h>
+#include <dxgi1_2.h>
+
+int capture_frame();
+#endif //GAMECLIENTSDL_CAPTURE_H
